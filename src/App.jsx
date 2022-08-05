dd1766f907b4db94c5948e9f8dbe12afe28286a8
JoaoLeite47 <StalkerJJ@outlook.com>
2022-08-05 19:15:31 -0300

    feat<topbar> menu hamburguer implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Topbar from "./components/topbar/Topbar";
 import Intro from "./components/Intro/Intro";
 import Cliente from "./components/Cliente/Cliente";
 import Alocacao from "./components/Alocacao/Alocacao";
 import Carros from "./components/Carros/Carros";
 import Categoria from "./components/Categoria/Categoria";
-import "./App.css"
+import "./App.css";
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div className="app">
-      <Topbar />
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className="sections">
         <Intro />
         <Cliente />
