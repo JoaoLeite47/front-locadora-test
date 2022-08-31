b3ada77412ad711c71b63ce4b28e9a78a65ce951
JoaoLeite47 <StalkerJJ@outlook.com>
2022-08-31 15:51:15 -0300

    feat<FormModalAlocacao> create method implemented

--- src/components/FormModalAlocacao/FormModalAlocacao.jsx
+++ src/components/FormModalAlocacao/FormModalAlocacao.jsx
@@ -1,15 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import "./FormModalAlocacao.css";
 
 export default function FormModalAlocacao() {
+  const [cpf_fk, setCpf_fk] = useState("");
+  const [chassi_fk, setChassi_fk] = useState("");
+  const [dt_saida, setDt_saida] = useState("");
+  const [dt_entrega, setDt_entrega] = useState("");
+
+  const submitHandler = () => {
+    const data = { cpf_fk, chassi_fk, dt_saida, dt_entrega };
+    try {
+      fetch("http://localhost:5000/alocacao/novo", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      }).then(() => {
+        alert("Operação concluida!");
+      });
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  const teste = () => {
+    console.log(cpf_fk, chassi_fk, dt_saida, dt_entrega);
+  };
+
   return (
-    <form>
-      <input type="text" name="cpf_fk" placeholder="Cpf" required />
-      <input type="text" name="chassi_fk" placeholder="Chassi" required />
-      <input type="date" name="dt_saida" placeholder="Data de saída" required />
+    <form onSubmit={submitHandler} onChange={teste}>
+      <input
+        type="text"
+        name="cpf_fk"
+        value={cpf_fk}
+        onChange={(e) => {
+          setCpf_fk(e.target.value);
+        }}
+        placeholder="Cpf"
+        required
+      />
+      <input
+        type="text"
+        name="chassi_fk"
+        value={chassi_fk}
+        onChange={(e) => {
+          setChassi_fk(e.target.value);
+        }}
+        placeholder="Chassi"
+        required
+      />
+      <input
+        type="date"
+        name="dt_saida"
+        value={dt_saida}
+        onChange={(e) => {
+          setDt_saida(e.target.value);
+        }}
+        placeholder="Data de saída"
+        required
+      />
       <input
         type="date"
         name="dt_entrega"
+        value={dt_entrega}
+        onChange={(e) => {
+          setDt_entrega(e.target.value);
+        }}
         placeholder="Data de entrega"
         required
       />
