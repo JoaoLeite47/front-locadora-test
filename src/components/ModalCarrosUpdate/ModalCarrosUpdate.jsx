d85cf2078e2a16b98c916f589dc9c424dc03d528
JoaoLeite47 <StalkerJJ@outlook.com>
2022-09-01 13:55:38 -0300

    feat<FormModalCarrosUpdate> form has implemented

--- src/components/ModalCarrosUpdate/ModalCarrosUpdate.jsx
+++ src/components/ModalCarrosUpdate/ModalCarrosUpdate.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import FormModalCarrosUpdate from "../FormModalCarrosUpdate/FormModalCarrosUpdate";
 import "./ModalCarrosUpdate.css";
 
 export default function ModalCarrosUpdate({ onClose = () => {} }) {
@@ -6,7 +7,9 @@ export default function ModalCarrosUpdate({ onClose = () => {} }) {
     <div className="carroModalUpdate">
       <div className="carroModalUpdateContainer">
         <button className="carroModalUpdateClose" onClick={onClose} />
-        <div className="carroModalUpdateContant"></div>
+        <div className="carroModalUpdateContant">
+          <FormModalCarrosUpdate/>
+        </div>
       </div>
     </div>
   );
