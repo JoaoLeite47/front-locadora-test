5c24f65f62877753b696033316e0569f4fb0561c
JoaoLeite47 <StalkerJJ@outlook.com>
2022-09-02 13:19:00 -0300

    feat<ModalAlocacaoUpdate> new form implemented

--- src/components/ModalAlocacaoUpdate/ModalAlocacaoUpdate.jsx
+++ src/components/ModalAlocacaoUpdate/ModalAlocacaoUpdate.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./ModalAlocacaoUpdate.css";
+import { ImSearch } from "react-icons/im";
 
 export default function ModalAlocacaoUpdate({ onClose = () => {} }) {
   return (
@@ -7,6 +8,42 @@ export default function ModalAlocacaoUpdate({ onClose = () => {} }) {
       <div className="alocacaoModalUpdateContainer">
         <button className="alocacaoModalUpdateClose" onClick={onClose} />
         <div className="alocacaoModalUpdateContant">
+          <div className="searchColumn">
+            <input
+              type="text"
+              className="searchBar"
+              placeholder="digite o id aqui..."
+            />
+            <button className="SearchButton">
+              <ImSearch size="1.5em" />
+            </button>
+          </div>
+          <div className="formUpdateAlocacao">
+            <form>
+              <input type="text" name="cpf_fk" placeholder="Cpf" required />
+              <input
+                type="text"
+                name="chassi_fk"
+                placeholder="Chassi"
+                required
+              />
+              <input
+                type="date"
+                name="dt_saida"
+                placeholder="Data de saída"
+                required
+              />
+              <input
+                type="date"
+                name="dt_entrega"
+                placeholder="Data de entrega"
+                required
+              />
+              <button className="update" type="submit">
+                Atualizar
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
