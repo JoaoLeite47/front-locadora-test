a5624fc1ab6716c5cc1ef2b687acfa10204536e0
JoaoLeite47 <StalkerJJ@outlook.com>
2022-09-02 13:02:11 -0300

    feat<ModalCarrosUpdate> new form for Cars Update

--- src/components/ModalCarrosUpdate/ModalCarrosUpdate.jsx
+++ src/components/ModalCarrosUpdate/ModalCarrosUpdate.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./ModalCarrosUpdate.css";
+import { ImSearch } from "react-icons/im";
 
 export default function ModalCarrosUpdate({ onClose = () => {} }) {
   return (
@@ -7,6 +8,41 @@ export default function ModalCarrosUpdate({ onClose = () => {} }) {
       <div className="carroModalUpdateContainer">
         <button className="carroModalUpdateClose" onClick={onClose} />
         <div className="carroModalUpdateContant">
+          <div className="searchColumn">
+            <input
+              type="text"
+              className="searchBar"
+              placeholder="digite o cpf aqui..."
+            />
+            <button className="SearchButton">
+              <ImSearch size="1.5em" />
+            </button>
+          </div>
+          <div className="formUpdateCarro">
+            <form>
+              <input type="text" name="chassi" placeholder="Chassi" required />
+              <input type="text" name="cor" placeholder="Cor" required />
+              <input
+                type="number"
+                name="modelo"
+                placeholder="Modelo"
+                required
+              />
+              <input type="text" name="marca" placeholder="Marca" required />
+              <input type="text" name="placa" placeholder="Placa" required />
+              <input type="number" name="ano" placeholder="Ano" required />
+              {/* <select name="categoria_fk" placeholder="Categoria" id="categoria_fk" required></select> */}
+              <input
+                type="number"
+                name="categoria_fk"
+                placeholder="Categoria"
+                required
+              />
+              <button className="update" type="submit">
+                Atualizar
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
